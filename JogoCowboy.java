60a74c8c1dcba08148803a657a30f2c8a8723094
Gustavo Muniz <gustavomunizruiz@hotmail.com>
2026-08-19 10:21:58 -0300

    alteracao

---
+++ JogoCowboy.java
@@ -0,0 +1,37 @@
+public class JogoCowboy {
+
+            String nome;
+            int vidas;
+            String poder;
+            String cor; 
+            int moedas;
+            int tempo; 
+            
+    public static void main(String[] args) {
+     }
+        public void pular (){
+        }
+
+        public void andar (){
+        }
+
+        public void correr (){
+        }
+
+        public void tomarDano (){
+        }
+
+        public void pisar (){
+        }
+
+        public void atirar (){
+        }
+
+        public void ganharMoeda (){
+        }
+
+        public void morrer (){
+        }
+
+}
+
